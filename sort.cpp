04c40dbb6970d3ecd13fb9dbc4a1d0bab84d6242
xiaojunhua <xiaojunhua@bytedance.com>
2024-07-07 20:26:57 +0800

    add some changes

--- sort.cpp
+++ sort.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 
 // This is a example application for C++
+void sort() {
+
+}
 
 int main(int argc, const char** argv) {
     std::cout << "Hello from " << argv[1] << ". I got " << argc << " arguments\n" << std::endl;
